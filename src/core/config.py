1480cf455be0870d2aad6e6cf0227de089eedb0e
Guilherme <domingos.guilherme@gmail.com>
2025-07-23 17:12:36 -0300

    feat: Redis is not beeing used anymore

--- src/core/config.py
+++ src/core/config.py
@@ -7,7 +7,6 @@ class Settings(BaseSettings):
     BASE_URL: str = Field(..., env="BASE_URL")
     LOG_API: Optional[str] = Field(default=None, env="LOG_API")
     LOG_PROJECT_ID: Optional[str] = Field(default=None, env="LOG_PROJECT_ID")
-    REDIS_URL: str = Field(..., env="REDIS_URL")
     SENTRY_DSN: Optional[str] = Field(default=None, env="SENTRY_DSN")
     MONGO_URI: str = Field(..., env="MONGO_URI")
     MONGO_DB: str = Field("intel", env="MONGO_DB")
--- src/core/redis.py
+++
@@ -1,9 +0,0 @@
-from .config import settings
-from redis.asyncio import Redis
-
-
-redis = Redis.from_url(
-    settings.REDIS_URL,
-    encoding="utf-8",
-    decode_responses=True
-)
