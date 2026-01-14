49790ea2bd8c6ac9f03788cb5a8748beda0febf3
Guilherme <domingos.guilherme@gmail.com>
2026-01-14 17:34:08 -0300

    feat: Add new env variables to standard config settings

--- src/core/config.py
+++ src/core/config.py
@@ -5,11 +5,20 @@ from typing import Optional
 
 class Settings(BaseSettings):
     BASE_URL: str = Field(..., env="BASE_URL")
+    APP_NAME: str = Field("Link Shortener API", env="APP_NAME")
+    APP_ENV: str = Field("dev", env="ENV")
+    HOST: str = Field("0.0.0.0", env="HOST")
+    PORT: int = Field(8000, env="PORT")
+
     LOG_API: Optional[str] = Field(default=None, env="LOG_API")
+    LOG_API_KEY: Optional[str] = Field(default=None, env="LOG_API_KEY")
     LOG_PROJECT_ID: Optional[str] = Field(default=None, env="LOG_PROJECT_ID")
+
     SENTRY_DSN: Optional[str] = Field(default=None, env="SENTRY_DSN")
+
     MONGO_URI: str = Field(..., env="MONGO_URI")
     MONGO_DB: str = Field("intel", env="MONGO_DB")
+
     ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(60 * 24 * 8, env="ACCESS_TOKEN_EXPIRE_MINUTES")
     JWT_SECRET: str = Field(..., env="JWT_SECRET")
     JWT_ALGORITHM: str = Field("HS256", env="JWT_ALGORITHM")
